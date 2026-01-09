04a5012e549aa5441406fa8149ef10dcc44c3b1b
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-01-09 23:32:02 +0530

    shopping cart

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 let addToCart=document.querySelectorAll(".addtocart");
-const cartItemsContainer = document.getElementById('cart-items');
-const subtotalEl = document.getElementById('subtotal');
-let subTotal=0;
-const items=[
-    Air-Fryer,
-    oven,
-    frother,
-    Kettle ,
-]
+let cartItemsContainer = document.getElementById('cart-items');
+let products={
+    1:{name:"Air Fryer",price:31.67,img:"./assets/airFryer.png"},
+    2:{name:"Microwave oven",price:37.2,img:"./assets/oven.png"},
+    3:{name:"Coffee Frother",price:3.37,img:"./assets/frother.png"},
+    4:{name:"Electric Kettle",price:7.14,img:"./assets/download.png"},
+}
 
-addToCart.forEach(product=>{
-    let productName = product.getAttribute("data-name");
-
-    product.addEventListener("click", () =>{
-        product.innerText="Already added";
-          product.disabled = true;
-
-          
+addToCart.forEach(el=>{
+    el.addEventListener("click", () =>{
+        el.innerText="Added to cart";
+        //el.innerHTML = name; // shows the alert
+        //cartItemsContainer.innerHTML=<div class="cart"><p></p></div>
+        el.disabled=true;
+        updatecart();  
     });
 });
+function updatecart(){
+   let div=document.createElement("div");
+   div.id="cartDisplay";
+   div.innerHTML = `<h2>Your Cart</h2>`;
+   const ids=Object.keys[cart];
+   ids.forEach(id => {
+      let item=cart[id];
+      div.innerHTML += `
+            <div style="background:white; margin:10px; padding:15px; border:2px solid black; border-radius:10px; display:flex;gap:20px;">
+            <img src="${item.img}"width="120" height="120" style="border:1px solid black;padding:5px;border-radius:5px;">
+            </div>`
+   })
+}
+let buttons=document.querySelectorAll(".color");
